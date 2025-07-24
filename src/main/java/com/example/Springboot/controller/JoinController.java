4a1c654345614829ee10cb203b5cb00f6d8b5780
only4wxx <orchidbyw1@gmail.com>
2025-07-25 04:21:41 +0900

    [Feat] 실습 - 로그인 로직(jwt)

--- src/main/java/com/example/Springboot/controller/JoinController.java
+++ src/main/java/com/example/Springboot/controller/JoinController.java
@@ -1,10 +1,17 @@
 package com.example.Springboot.controller;
 
+import com.example.Springboot.config.PrincipalHandler;
+import com.example.Springboot.domain.Member;
 import com.example.Springboot.dto.request.JoinRequestDto;
+import com.example.Springboot.dto.response.TokenResponseDto;
+import com.example.Springboot.jwt.JwtTokenProvider;
 import com.example.Springboot.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class JoinController {
 
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/join")
     public void join(@RequestBody JoinRequestDto joinRequestDto) {
         memberService.join(joinRequestDto);
     }
 
+    @PostMapping("/login")
+    public TokenResponseDto login(@RequestBody JoinRequestDto joinRequestDto) {
+        Member member = memberService.login(joinRequestDto);
+        return TokenResponseDto.of(jwtTokenProvider.generateAccessToken(member.getName()), jwtTokenProvider.generateRefreshToken(member.getName()));
+    }
+
+    @GetMapping("/my")
+    public ResponseEntity<String> getMyName() {
+        String name = PrincipalHandler.getNameFromPrincipal();
+        return ResponseEntity.ok(name);
+    }
 }
